6a70598378ba6185f2a0ba1fab8e2ae475ef600b
Tony Robertson <tonyr729@gmail.com>
2018-10-17 09:11:20 -0600

    Add test to assert category

--- test/room_test.rb
+++ test/room_test.rb
@@ -4,10 +4,14 @@ require './lib/room'
 
 class RoomTest < MiniTest::Test
   def setup
-    @room = Room.new
+    @room = Room.new(:bedroom)
   end
 
   def test_it_exists
     assert_instance_of Room, @room
+  end
+
+  def test_it_has_a_category
+    assert_equal :bedroom, @room.category
   end
 end
