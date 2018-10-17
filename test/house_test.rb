25faccd0f7024a7019aeb71546b67f6ac1eec6be
Tony Robertson <tonyr729@gmail.com>
2018-10-17 09:25:55 -0600

    Add test to assert house address

--- test/house_test.rb
+++ test/house_test.rb
@@ -5,7 +5,7 @@ require './lib/house'
 
 class HouseTest < MiniTest::Test
   def setup
-    @house = House.new("$40000")
+    @house = House.new("$40000", "123 sugar lane")
   end
 
   def test_it_exists
@@ -14,5 +14,9 @@ class HouseTest < MiniTest::Test
 
   def test_it_has_a_price
     assert_equal 40000, @house.price
+  end
+
+  def test_it_has_an_address
+    assert_equal "123 sugar lane", @house.address
   end
 end
