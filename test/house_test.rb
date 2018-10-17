a10bf6e58b0bc199e927d0bc8f2adcdae81fcd08
Tony Robertson <tonyr729@gmail.com>
2018-10-17 10:06:09 -0600

    Add test to assert house can group rooms by category

--- test/house_test.rb
+++ test/house_test.rb
@@ -68,7 +68,22 @@ class HouseTest < MiniTest::Test
     @house.add_room(@room_2)
     @house.add_room(@room_3)
     @house.add_room(@room_4)
-
+    
     assert_equal [@room_4, @room_3, @room_2, @room_1], @house.rooms_sorted_by_area
+  end
+  
+  def test_it_can_group_rooms_by_category
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+
+    expected = {
+      :bedroom => [@room_1, @room_2],
+      :living_room => [@room_3],
+      :basement => [@room_4]
+    }
+    
+    assert_equal expected, @house.rooms_by_category
   end
 end
