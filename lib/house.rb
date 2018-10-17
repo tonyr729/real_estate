362f6329feec1e317d3c24472014212a32770ae7
Tony Robertson <tonyr729@gmail.com>
2018-10-17 09:27:57 -0600

    Add rooms instance varible as an array

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,10 @@
 class House
-  attr_reader :address
+  attr_reader :address, :rooms
 
   def initialize(price, address)
     @price = price
     @address = address
+    @rooms = []
   end
 
   def price
