3856aa40e123c4c5ad2270dece7e51bc9dfe5681
Tony Robertson <tonyr729@gmail.com>
2018-10-17 09:22:59 -0600

    Add test for price method to covert price to integer without symbols

--- test/house_test.rb
+++ test/house_test.rb
@@ -5,10 +5,14 @@ require './lib/house'
 
 class HouseTest < MiniTest::Test
   def setup
-    @house = House.new
+    @house = House.new("$40000")
   end
 
   def test_it_exists
     assert_instance_of House, @house
+  end
+
+  def test_it_has_a_price
+    assert_equal 40000, @house.price
   end
 end
