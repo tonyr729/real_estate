7100a84563410d66038fae48e868b27f7c1a3071
Tony Robertson <tonyr729@gmail.com>
2018-10-17 09:26:10 -0600

    Add house address attr_reader and param

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,11 @@
 class House
-  def initialize(price)
+  attr_reader :address
+
+  def initialize(price, address)
     @price = price
+    @address = address
   end
+
   def price
     @price.tr('$', '').to_i
   end
